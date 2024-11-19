a7e12b0cc75d9aedb422fdfae4c39f2c8fbb20fb
bmvaldivieso <bmvaldivieso@utpl.edu.ec>
2024-11-19 16:45:05 -0500

    second commit

--- user_management/users/forms.py
+++ user_management/users/forms.py
@@ -2,11 +2,26 @@ from django import forms
 from .models import User
 
 class RegistrationForm(forms.ModelForm):
-    password_confirm = forms.CharField(widget=forms.PasswordInput())
+    password_confirm = forms.CharField(
+        widget=forms.PasswordInput(),
+        label="Confirmar Contraseña"  # Alias personalizado para este campo
+    )
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password', 'telefono', 'rol']
+        labels = {
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
+            'email': 'Correo Electrónico',
+            'username': 'Nombre de Usuario',
+            'password': 'Contraseña',
+            'telefono': 'Teléfono',
+            'rol': 'Rol',
+        }
+        help_texts = {
+            'username': '',  # Elimina el texto de ayuda predeterminado
+        }
 
     def clean_password_confirm(self):
         password = self.cleaned_data.get('password')
